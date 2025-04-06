10427d37a79a87adb43a063a7e4224897343b095
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-04-07 00:22:42 +0530

    started learning react using react.dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+/*************  ✨ Windsurf Command 🌟  *************/
+import React from "react";
+import "./App.css";
+function MyButton() {
+  return <button>Button</button>;
 }
+const person = {
+  name: "lakshman",
+  age: 19,
+  gender: "male",
+  imageurl: "./src/assets/react.svg",
+  imagessize: 90,
+  ismarried: false,
+};
+let status = person.ismarried;
+let checkmarried = {
+  content: status ? "single" : "Mingle",
+};
+export default function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <MyButton />
+      <br />
+      <MyButton />
+      <br />
+      <img
+        src={person.imageurl}
+        alt={"loading.."}
+        style={{ width: person.imagessize, height: person.imagessize }}
+      />
+      <h1>{checkmarried.content}</h1>
+    </div>
+  );
+};
 
-export default App
+/*******  b0545715-81d0-4b46-bc99-da8c746b1c7f  *******/
