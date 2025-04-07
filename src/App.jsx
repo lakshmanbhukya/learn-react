ec3fab091a9264379fe25a702f739d32ddf3a0a8
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-04-08 00:12:16 +0530

    Completed React Starter Guide -- commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-/*************  ✨ Windsurf Command 🌟  *************/
 import React from "react";
+import { useState } from "react";
 import "./App.css";
+import luffyimg from "./assets/luffyy.jpg";
 function MyButton() {
   return <button>Button</button>;
 }
@@ -11,12 +12,47 @@ const person = {
   imageurl: "./src/assets/react.svg",
   imagessize: 90,
   ismarried: false,
+  hasLaptop: true,
 };
 let status = person.ismarried;
 let checkmarried = {
   content: status ? "single" : "Mingle",
 };
+let laptop = person.hasLaptop;
+let laptopcontent = {
+  contentlist: laptop ? "Laptop" : "Buy Laptop",
+};
+// Rendering lists
+const products = [
+  { id: 1, name: "Apple", weight: 100, isavailable: true },
+  { id: 2, name: "banana", weight: 100, isavailable: false },
+  { id: 3, name: "watermelon", weight: 100, isavailable: true },
+  { id: 4, name: "mango", weight: 100, isavailable: false },
+  { id: 5, name: "sapota", weight: 100, isavailable: true },
+];
+const porductitems = products.map((product) => (
+  <li key={product.id} style={{ color: product.isavailable ? "green" : "red" }}>
+    {product.name}
+  </li>
+));
+//Responding to events
+function clicked() {
+  alert("Button handled here");
+}
+function PlaceImge() {
+  return (
+    <img
+      src={luffyimg}
+      alt="luffy"
+      style={{ width: "400px", height: "250px",margin:"10px"}}
+    />
+  );
+}
 export default function App() {
+  const [count, setcount] = useState(0);
+  function countclick() {
+    setcount(count + 1);
+  }
   return (
     <div>
       <h1>hello world</h1>
@@ -30,8 +66,22 @@ export default function App() {
         style={{ width: person.imagessize, height: person.imagessize }}
       />
       <h1>{checkmarried.content}</h1>
+      <h1>{laptopcontent.contentlist}</h1>
+      <ul type="none">{porductitems}</ul>
+      {/* //React will call your component function again. This time, count will be 1. Then it will be 2. And so on.
+
+If you render the same component multiple times, each will get its own state. Click each button separately */}
+      <button onClick={clicked}>Click Me !!</button>
+      {/* <button onClick={countclick}>button click : {count}</button> */}
+      <h1> count both at the same time</h1>
+      <MyButton count={count} onClick={countclick} />
+      <MyButton count={count} onClick={countclick} />
+      <div>
+        <div className="gallery">{PlaceImge()} {PlaceImge()}{PlaceImge()}</div>
+      </div>
     </div>
   );
-};
-
-/*******  b0545715-81d0-4b46-bc99-da8c746b1c7f  *******/
+  function MyButton({ count, onClick }) {
+    return <button onClick={onClick}> count {count}</button>;
+  }
+}
