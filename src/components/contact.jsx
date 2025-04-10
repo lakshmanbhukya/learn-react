da64e114aab4ed6590a4cdfbffde1a7887ca3852
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-04-10 22:00:31 +0530

    handing fomrs in react --commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Hello ${name} Age ${age} email ${email}`);
+  };
+
   return (
     <>
-      <h1>Hello This is Contact Page !!</h1>
-      <p>you are rotued here react-rotuer-dom ?? </p>
-      <h3>Thanks !!</h3>
+      <form onSubmit={handleSubmit} align="center">
+        <label htmlFor="name">name :</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        /> <br/><br/>
+        <label htmlFor="age">age :</label>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        /><br/><br/>
+        <label htmlFor="email">email :</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br/>
+        <label htmlFor="password">password :</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /><br/>
+        <button type="submit">Greet !!</button>
+      </form>
     </>
   );
 }
--- src/components/notfound-404.jsx
+++ src/components/notfound-404.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 const NotFound = () => {
-    return (
-      <div style={{
+  return (
+    <div
+      style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-      }}>
-        <h1 style={{ color: "red", fontSize: "50px" }}>404 - Page Not Found</h1>
-      </div>
-    );
-  };
-  
-  export default NotFound;
-  
+      }}
+    >
+      <h1 style={{ color: "red", fontSize: "50px" }}>404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
