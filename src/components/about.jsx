0ad588786bd810f0b7b64281082938a0294bf847
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-04-11 20:01:23 +0530

    installed Tailwind css -- commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "../index.css";
 export default function About() {
   return (
     <>
-      <h1>About Page </h1>
+      <div className="text-center p-10"></div>
+      <h1 className="text-3xl font-bold text-blue-600">About Page </h1>
       <p>You are routed to About Page </p>
       <h3>Thank you !! </h3>
     </>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"], // ✅ add jsx
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
